1979594e8894bc446d980516055b397ff11c7281
gittykite <gittykite@users.noreply.github.com>
2019-11-30 16:33:01 +0900

    Update 2_hello_deploy.js
    HelloThere contract added

--- hello-trf/migrations/2_hello_deploy.js
+++ hello-trf/migrations/2_hello_deploy.js
@@ -1,6 +1,8 @@
 var helloWorld = artifacts.require("HelloWorld");
+var helloThere = artifacts.require("HelloThere");
 
 module.exports = function(deployer,network){
     //console.log(network);
     deployer.deploy(helloWorld, "Hello,World!");
+    deployer.deploy(helloThere);
 }
