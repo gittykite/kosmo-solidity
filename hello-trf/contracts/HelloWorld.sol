ec0b77f4c968d1288810fdd4afdae5e270cde050
gittykite <gittykite@users.noreply.github.com>
2019-11-30 15:07:15 +0900

    create HelloWorld.sol

---
+++ hello-trf/contracts/HelloWorld.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.11;
+// pragma solidity >=0.4.22 <0.6.0;
+
+contract HelloWorld {
+
+    // state variable
+    string public greeting;
+
+    constructor(string memory _greeting) public{
+        greeting = _greeting;
+    }
+
+    function setGreeting(string memory _greeting) public {
+        greeting = _greeting;
+    }
+
+    // view function
+    function say() public view returns(string memory){
+        return greeting;
+    }
+}
