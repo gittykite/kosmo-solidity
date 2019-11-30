589a74e48971ab62764cc044628c214578c052d6
gittykite <gittykite@users.noreply.github.com>
2019-11-30 16:33:37 +0900

    Update test_Hello.js
    HelloThere contract test added

--- hello-trf/test/test_Hello.js
+++ hello-trf/test/test_Hello.js
@@ -1,8 +1,9 @@
-var helloWorld= artifacts.require("HelloWorld");
+var helloWorld = artifacts.require("HelloWorld");
+var helloThere = artifacts.require("HelloThere");
 
 contract("HelloWorld",function(accounts){
 
-    it("should be same as constructor argument", function (){
+    it("should be same as constructor argument1", function (){
         return helloWorld.deployed().then(function(instance){
             // check the result of say() => greeting
             return instance.say().then(function(greeting){
@@ -12,10 +13,31 @@ contract("HelloWorld",function(accounts){
     });
 
     // same test as above 
-    it("should be same as constructor argument", async function (){
+    it("should be same as constructor argument2", async function (){
         const hello = await helloWorld.deployed();
         const greeting = await hello.say();
         assert.equal(greeting, "Hello, World!");
+    });
+
+});
+
+contract("HelloThere",function(accounts){
+
+    it("should be same as constructor argument3", function (){
+        return helloThere.deployed().then(function(instance){
+            // check the result of say() => greeting
+            return instance.say().then(function(greeting){
+                assert.equal(greeting, "Hello, there!");
+            })
+        })
+    });
+
+    // same test as above 
+    it("should be same as constructor argument4", async function (){
+        const helloT = await helloThere.deployed();
+        const greetingT = await helloT.say();
+        console.log(greetingT);
+        assert.equal(greetingT, "Hello, there!");
     });
 
 });
